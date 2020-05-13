68199c38111b9e41586aeb1a64f1b9e3b44e4c4a
merihnguse <merih718632@gmail.com>
2020-05-13 21:32:34 +0200

    Julia house cost add

--- javascript/javascript1/week1/houseprice.js
+++ javascript/javascript1/week1/houseprice.js
@@ -2,7 +2,6 @@
 let volumeInMeters=8*10*10;
 let gardenSizeInM2=100;
 let peterHousePrice=2500000;
-let juliaHousePrice=1000000;
 
 let housePrice= volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 
@@ -11,4 +10,18 @@ if (housePrice > peterHousePrice) {
     console.log('peter is paying to much');
 } else {
     console.log('peter is paying to little');
+}
+//Julai house estimator
+let volumeInMeter=5*11*8;
+let gardenSizeInM=70;
+let juliaHousePrice=1000000;
+
+let houseCost= volumeInMeter * 2.5 * 1000 + gardenSizeInM * 300;
+
+
+console.log(houseCost);
+if (houseCost > juliaHousePrice) {
+    console.log('Julia is paying to much');
+} else {
+    console.log('Julia is paying to little');
 }
