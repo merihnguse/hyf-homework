e8f02cfcf1b81fef184e2589b88e93715951c731
merihnguse <merih718632@gmail.com>
2020-05-13 20:49:55 +0200

    dogage calculator

---
+++ javascript/javascript1/week1/dogage.js
@@ -0,0 +1,9 @@
+// dog age calculator
+var dogYearOfBirth= 2018;
+var dogYearFuture = 2045;
+
+var dogYear = dogYearFuture - dogYearOfBirth;
+
+console.log(dogYear);
+var shouldShowResultInDogYear = true;
+
