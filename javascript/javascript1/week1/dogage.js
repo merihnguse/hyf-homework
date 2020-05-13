c46210bb33e9f80071b3f2e3986d047a0c05fe3b
merihnguse <merih718632@gmail.com>
2020-05-13 21:01:41 +0200

    if (shouldShowResultInDogYear)

--- javascript/javascript1/week1/dogage.js
+++ javascript/javascript1/week1/dogage.js
@@ -5,5 +5,11 @@ var dogYearFuture = 2045;
 var dogYear = dogYearFuture - dogYearOfBirth;
 
 console.log(dogYear);
-var shouldShowResultInDogYear = true;
+var shouldShowResultInDogYear = false;
+
+if (shouldShowResultInDogYear) {
+    console.log('Your dog will be ' + 10 + ' human year in ' + dogYearFuture);
+} else {
+    console.log('Your dog will be ' +10*7 + ' dog years in ' + dogYearFuture );
+}
 
