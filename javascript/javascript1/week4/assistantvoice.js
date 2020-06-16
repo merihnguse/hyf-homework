7b9ffe2dd3896e579dd90d1c471b527450e611a1
merihnguse <merih718632@gmail.com>
2020-06-16 10:52:18 +0200

    I renew it

--- javascript/javascript1/week4/assistantvoice.js
+++ javascript/javascript1/week4/assistantvoice.js
@@ -8,14 +8,24 @@ function getReply(order) {
 		name = order.replace('hello my name is', '');
 		return `Nice to meet you ${name}`;
 	}
-	} else if (order === 'Remove fishing from my todo') {
-		return 'Removed fishing from your todo';
-	} else if (order === 'What is on my todo') {
-		let myTodo = [];
-		myTodo.push('fishing', 'Singing in the shower', 'Orange');
-		console.log(myTodo);
-		return 'You have fishing, signing and orange';
-	} else if (order.replace('What day is today?')) {
+	if (order.includes('what is your name?') && name != givingName) {
+		let name;
+		let givingName;
+		console.log('Who are you?');
+	} else if (order.includes('what is your name?') && name === givingName) {
+		console.log('I am ' + givingName);
+	}
+	if (order.startsWith('Adding') && order.endsWith('todo')) {
+		const todos = order.replace('Adding', '').replace('todo', '');
+		todo.push(todos);
+		return `${todo} is added`;
+	}
+	if (order.startsWith('Removing') && order.endsWith('my todo')) {
+		const todos = order.replace('Adding', '').replace('todo', '');
+		todo.splice(todo.indexOf(2));
+		return `removed ${todos}`;
+	}
+	if (order.includes('What day is today?')) {
 		//Array of date, month and year
 		return new Date().toLocaleDateString('en-US', {
 			weekday: 'long',
@@ -29,9 +39,11 @@ function displayMessage() {
 	console.log('Timer is done');
 }
 
-console.log(getReply('What is your name'));
+console.log(getReply('hello my name is Merih'));
+console.log(getReply('hello my name is'));
+console.log(getReply('What is your name?'));
 console.log(getReply('Adding fishing to my todo'));
-console.log(getReply('Remove fishing from my todo'));
-console.log(getReply('What is on my todo'));
+console.log(getReply('Removing fishing from my todo'));
 console.log(getReply('What day is today?'));
+//console.log(getReply('Timer is done'));
 setTimeout(displayMessage, 10000);
